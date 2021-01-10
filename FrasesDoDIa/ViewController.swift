0c936bf6aad77aca0860dec74c923f5de107cd73
Renato <renato.vieira@stant.com.br>
2021-01-10 00:45:11 -0300

    Initial Commit

---
+++ FrasesDoDIa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrasesDoDIa
+//
+//  Created by Renato on 1/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
