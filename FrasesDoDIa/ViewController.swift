5391bfefddf6dacd062e8eb0d19c4caa34bf36f1
Renato <renato.vieira@stant.com.br>
2021-01-17 00:54:05 -0300

    Addition of components and methods

--- FrasesDoDIa/ViewController.swift
+++ FrasesDoDIa/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var messages = ["Ter sucesso é falhar repetidamente, mas sem perder o entusiasmo",
+                    "Se você traçar metas absurdamente altas e falhar, seu fracasso será muito melhor que o sucesso de todos",
+                    "Quem sobrevive é o mais disposto a mudança"]
 
+    @IBOutlet weak var messageDayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.generationNumberAleatory()
     }
 
-
+    @IBAction func newMessageButton(_ sender: Any) {
+        self.generationNumberAleatory()
+    }
+    
+    public func generationNumberAleatory() {
+        let numberAleatory = arc4random_uniform(3)
+        
+        messageDayLabel.text = messages[Int(numberAleatory)]
+    }
 }
 
